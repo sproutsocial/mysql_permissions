4ef06c491645397c86b45312f480aeb614e2b651
Nicholas Flink <nicholas@sproutsocial.com>
2017-02-28 20:01:47 -0600

    Bump version: 0.1.2 → 0.1.3

--- ldap_mysql_granter/__init__.py
+++ ldap_mysql_granter/__init__.py
@@ -1,4 +1,4 @@
 # dont import any packages from setup.py's install_requires, or it will break
 # versioning
-# 0.1.2 - Mysql supported first
-__version__ = "0.1.2"
+# 0.1.3 - Mysql supported first
+__version__ = "0.1.3"
